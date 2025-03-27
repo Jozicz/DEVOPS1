d826d164960c62a8a3791f237ba793875e91b626
Sebastian <JoziczPeter@gmail.com>
2025-03-27 16:37:32 +0100

    Testy 3

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,8 +1,18 @@
+import pytest
+
 def add(a,b):
     return a+b
 
 def test_add():
     assert add(2,3) == 5
 
-def test_add():
-    assert add(2,3) == 55
+# def test_add_bad():
+#     assert add(2,3) == 55
+
+@pytest.mark.parametrize("a,b,wynik",[
+    (2,3,5),
+    (5,7,12),
+    (4,9,13)
+])
+def test_param_add(a,b,wynik):
+    assert add(a,b) == wynik
